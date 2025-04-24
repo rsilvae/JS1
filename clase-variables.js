dc2a3d114f633491e4b68ab3c38db4a7cee1545c
fitos <fitosilvae@hotmail.com>
2025-04-23 22:08:05 -0400

    clase2 variables

---
+++ clase-variables.js
@@ -0,0 +1,15 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+
+// Lo NO permitido
+
+let c = "Woody"
+let cda = "Woody"
+let pcAndy = "Woody"
+
+// Lo permitido
+
+let primerTrasteoDeAndy = "Woody"
+let urlDelUsuario = "google.com"
+let idDelUsuario = "2343452"

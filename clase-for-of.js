be6389ced44caa190cc2f0e0d1704188eb25093a
fitos <fitosilvae@hotmail.com>
2025-04-24 19:30:47 -0400

    clase 12 for of

---
+++ clase-for-of.js
@@ -0,0 +1,12 @@
+// for (variable of arr){
+//     codigo
+// }
+
+let canasta = ["Manzana", "Pera", "Naranja", "Uva"]
+
+for (fruta of canasta) {
+    console.log(fruta)
+}
+for (fruta of canasta[1]) {
+    console.log(fruta)
+}

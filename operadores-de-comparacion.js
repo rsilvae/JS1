d8b0be5348dba21fbc6fdac71e59f0fab6e1e7dc
fitos <fitosilvae@hotmail.com>
2025-04-24 14:15:46 -0400

    clase7 operadores de comparacion

---
+++ operadores-de-comparacion.js
@@ -0,0 +1,23 @@
+// Operadores de comparacion
+
+// == es igual a ( en valor)
+
+// === es igual a ( en valor y tipo de dato)
+
+// !=  es diferente a  ( en valor)
+
+// !==  es diferente a  ( en valor y dato)
+
+//  > es mayor que 
+
+// < es menor que 
+
+//  >= es mayor o igual que 
+
+// <= es menor o igual que
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+a == b

8c2fe7f6755136690da3910590cb0e116aab1cc7
fitos <fitosilvae@hotmail.com>
2025-04-23 22:17:48 -0400

    clase3 tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,45 @@
+// Datos Primitivos
+
+// string
+
+let nombre = "Fito"
+
+// number
+
+let edad = 26
+
+// boolean
+
+let esMayorDeEdad = true
+
+// null
+
+let noHayValor = null
+
+// Undefined
+
+let noDefinido = undefined
+
+// symbol
+let simboloUnico = Symbol('unico')
+
+// bigint
+
+let numeroGrande = 2n
+
+// Datos Complejos
+
+// objeto
+
+let carro = {
+    marca: "tesla",
+    modelo: 'model S'
+}
+
+// array
+
+let frutas = ["manzana", "uva", "sandia"]
+
+// function
+
+function saludar() { }

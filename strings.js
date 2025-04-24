be94a255f12a1baccd92d980fcbdfb2d5e24ed93
fitos <fitosilvae@hotmail.com>
2025-04-23 23:04:59 -0400

    clase4 tipos de variables

---
+++ strings.js
@@ -0,0 +1,17 @@
+// Tipos de dato: string
+
+let string1 = 'hola'
+let string2 = "java"
+let string3 = `${string1} mundo`
+let string4 = string1 + " " + string2
+
+// console.log(string1)
+// console.log(string2)
+// console.log(string3)
+// console.log(string4)
+
+let frase = 'JavaScript es Extremadamente Genial'
+console.log(frase.length)
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())
+console.log(frase.substring(0, 10))

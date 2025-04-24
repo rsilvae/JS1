a745b4aa805bd5d0aefc2b29a65cda721e7cc158
fitos <fitosilvae@hotmail.com>
2025-04-24 17:40:58 -0400

    clase 9 if

---
+++ clase-if.js
@@ -0,0 +1,10 @@
+let nombre = "caca"
+
+
+if (nombre === "Rodolfo") {
+    console.log("Hola Rodolfo")
+} else if (nombre === "Diego") {
+    console.log("Hola Diego")
+} else {
+    console.log("Quien soy tu?")
+}

0fc6c23452f7a10a6502f7dbef1555449fd1130f
fitos <fitosilvae@hotmail.com>
2025-04-24 20:16:57 -0400

    clase15 do while

---
+++ clase-while-do.js
@@ -0,0 +1,11 @@
+
+// Lo que diferencia al do while de el while es que el 
+// do while ejecutara el codigo antes de comprobar la condicion
+
+
+let contador = 0
+
+do {
+    console.log(contador)
+    contador++
+} while (contador < 1)

c3c25067819152c1d4173db12c90fb729a4dafee
fitos <fitosilvae@hotmail.com>
2025-05-01 23:56:11 -0400

    clase 25

---
+++ clase-poo.js
@@ -0,0 +1,27 @@
+/*
+
+estructura de datos
+
+key / value 
+
+objeto {
+  propiedad: valor, 
+  propiedad: valor, 
+  propiedad: valor 
+  Metodos()
+}
+
+*/
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    saludar() {
+        console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} aÃ±os.`);
+    }
+}
+
+const persona1 = new Persona("Mariana", 25);
+
+persona1.saludar();
